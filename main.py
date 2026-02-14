cafdeabeafc54d51080b7f20dca7bd3b260836c1
prithvihn <135147281+prithvihn@users.noreply.github.com>
2026-02-14 17:45:51 +0530

    render link+vercel

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 #cors
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://gpt-frontend-sable.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
